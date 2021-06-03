dd2ec89fd8113c83c9e8908a61b09f47c22e1c61
Liw1sa2Yiu <qiangduan1993@126.com>
2021-06-03 12:58:40 +0800

    1

---
+++ frontend/src/main/java/com/springcloud/frontend/controller/HelloController.java
@@ -0,0 +1,15 @@
+package com.springcloud.frontend.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+	
+	@GetMapping("/hello")
+	public String test(){
+	
+		return "hello";
+	}
+}
